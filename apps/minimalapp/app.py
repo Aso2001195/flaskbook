cea93d4346386ef4ac74c7251412e7658d40b5c9
Aso2001195 <2001195@s.asojuku.ac.jp>
2022-08-16 03:10:32 +0900

    current_app stack_access & g Temporary space

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, current_app, g, render_template, url_for
 
 app = Flask(__name__)
 
@@ -22,3 +22,12 @@ with app.test_request_context():
     print(url_for("index"))
     print(url_for("hello-endpoint", name="world"))
     print(url_for("show_name", name="ichiro", page="1"))
+
+
+ctx = app.app_context()
+ctx.push()
+
+print(current_app.name)
+
+g.connection = "connection"
+print(g.connection)
